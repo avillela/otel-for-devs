ea825096e0bf1fec72e65784c9a1eb32dcc84798
Adriana Villela <indycomp0t1@gmail.com>
2026-04-02 22:01:34 +0000

    Install Homebrew and install otel-tui

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -12,3 +12,10 @@ sed -i -e "s/#alias ll='ls -l'/alias ll='ls -al'/g" ~/.bashrc
 ### -------------------
 
 curl -L -O https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/latest/download/opentelemetry-javaagent.jar
+
+### -------------------
+### Install Homebrew and otel-tui
+### -------------------
+
+./install-homebrew.sh
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" && sudo /home/linuxbrew/.linuxbrew/bin/brew install ymtdzzz/tap/otel-tui
